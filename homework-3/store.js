f50b40cd57838c63c9b544a540aab4690ccb9bd1
Julianna Eyster <jieyster@iu.edu>
2025-02-02 10:48:25 -0500

    rounded prices

--- homework-3/store.js
+++ homework-3/store.js
@@ -4,7 +4,7 @@ const tax = .07;
 // tax shouldn't change
 const total = price * (1 + tax);
 // equation shouldn't change
-console.log("total", total);
+console.log("total", (total).toFixed(2));
 
 console.log("qualifies?", price > 50);
 
@@ -12,4 +12,4 @@ let discount = .10;
 // discount might change
 const discountTotal = price * (1 + tax) * (1 - discount);
 // equation shouldn't change
-console.log("discount total", discountTotal);
+console.log("discount total", (discountTotal).toFixed(2));
